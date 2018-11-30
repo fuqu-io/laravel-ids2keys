bcb1b537f6dc4b1cf0c3dd3b012b1095ded05e2e
Tarek Adam <tarek.adam@gmail.com>
2018-11-30 09:43:31 -0600

    touch bootViews

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -41,6 +41,8 @@ class ServiceProvider extends BaseServiceProvider{
 	private function bootConfig(){
 		$this->publishes([__DIR__ . '/../config/main.php' => config_path(SELF::SHORT_NAME . '.php')]);
 		//$this->mergeConfigFrom(__DIR__ . '/../config/main.php', SELF::SHORT_NAME);
+
+
 	}
 
 	/**
